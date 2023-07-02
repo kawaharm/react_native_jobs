e1a33ff844572b6ae95ab145ec1f3e5c6c6d2a09
kawaharm <mjbk295@gmail.com>
2023-07-01 17:52:27 -0700

    finished welcome page

--- app/_layout.js
+++ app/_layout.js
@@ -1,7 +1,31 @@
 import { Stack } from "expo-router";
+// useCallback: cache a function definition between re-renders
+import { useCallback } from "react";
+// UseFonts: required for using custom fonts
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+
+// Show splash screen until hideAsync is called
+SplashScreen.preventAutoHideAsync();
 
 const Layout = () => {
-  return <Stack />;
+  // Import fonts
+  const [fontsLoaded] = useFonts({
+    DMBold: require("../assets/fonts/DMSans-Bold.ttf"),
+    DMMedium: require("../assets/fonts/DMSans-Medium.ttf"),
+    DMRegular: require("../assets/fonts/DMSans-Regular.ttf"),
+  });
+
+  // Show homepage if fonts loaded successfully
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) return null;
+
+  return <Stack onLayout={onLayoutRootView} />;
 };
 
 export default Layout;
