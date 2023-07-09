b137cfc8b90ee2d05ddfc7b706116b2c1e56e7c1
kawaharm <mjbk295@gmail.com>
2023-07-09 14:16:01 -0700

    added search

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -30,7 +30,11 @@ const JobDetails = () => {
   const [refreshing, setRefreshing] = useState(false);
   const [activeTab, setActiveTab] = useState(tabs[0]);
 
-  const onRefresh = () => {};
+  const onRefresh = useCallback(() => {
+    setRefreshing(true);
+    refetch();
+    setRefreshing(false);
+  }, []);
 
   const displaytabContent = () => {
     switch (activeTab) {
